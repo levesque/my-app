30fd330ec5e2979b9a6d337a5d1607b60de56d2b
Laverne Kelly <levesque09@gmail.com>
2021-11-24 12:22:16 -0700

    handicap wip

--- src/index.js
+++ src/index.js
@@ -2,137 +2,169 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const HOME_PAGE = "Handicap";
+const ROUNDS_PAGE = "Rounds";
+const COURSES_PAGE = "Courses";
 
-class FilterableProductTable extends React.Component {
+class HandicApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            rawData: this.props.rawData,
-            filterText: "",
-            inStockOnly: false,
+            selectedTab: HOME_PAGE,
         };
-        this.handleOnFilterChange = this.handleOnFilterChange.bind(this);
-        this.handleCheckChanged = this.handleCheckChanged.bind(this);
+        this.handleTabClick = this.handleTabClick.bind(this);
     }
 
-    handleOnFilterChange(filterText) {
-        this.setState({ filterText: filterText });
+    handleTabClick(tabText) {
+        this.setState({ selectedTab: tabText });
     }
 
-    handleCheckChanged(checked){
-        this.setState({inStockOnly: checked});
+    render() {
+
+        let pageContents;
+        switch (this.state.selectedTab) {
+            case HOME_PAGE:
+                pageContents = <HomePage courses={this.props.courses} />
+                break;
+
+            case ROUNDS_PAGE:
+                pageContents = <p>{ROUNDS_PAGE}</p>
+                break;
+
+            case COURSES_PAGE:
+                pageContents = <CoursesPage courses={this.props.courses} />
+                break;
+
+            default:
+                pageContents = <p>Oopsie, unrecognized tab name</p>
+                break;
+        }
+        return (
+            <div>
+                <div>
+                    <TabButton value={HOME_PAGE} handleOnClick={this.handleTabClick} />
+                    <TabButton value={ROUNDS_PAGE} handleOnClick={this.handleTabClick} />
+                    <TabButton value={COURSES_PAGE} handleOnClick={this.handleTabClick} />
+                </div>
+                <div>
+                    {pageContents}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCourse: "",
+        };
+        this.handleOnCourseChange = this.handleOnCourseChange.bind(this);
+    }
+
+    handleOnCourseChange(courseName) {
+        this.setState({ selectedCourse: courseName });
+        // TODO set slope/rating etc
+    }
+
+    render() {
+
+        const justCourses = [...new Set(this.props.courses.map(item => item.course))];
+        var courseOptions = [];
+        for (var k = 0; k < justCourses.length; k++) {
+            courseOptions.push(<option key={justCourses[k]} value={justCourses[k]}> {justCourses[k]} </option>);
+        }
+        return (
+            <div>
+                <p>
+                    HandicApp
+                </p>
+                <select id='courseSelect'>
+                    {courseOptions}
+                </select>
+            </div>
+        );
+    }
+}
+
+
+class CoursesPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCourse: "",
+        };
+        this.handleDeleteCourseClicked = this.handleDeleteCourseClicked.bind(this);
+    }
+
+    handleDeleteCourseClicked(courseName) {
+        this.setState({ selectedCourse: courseName });
+    }
+
+    render() {
+
+        var tableRows = [];
+        for (var i = 0; i < this.props.courses.length; i++) {
+            tableRows.push(
+                <tr>
+                    <td>{this.props.courses[i].course}</td>
+                    <td>{this.props.courses[i].tee}</td>
+                    <td>{this.props.courses[i].rating}</td>
+                    <td>{this.props.courses[i].slope}</td>
+                </tr>
+            );
+        }
+
+        return (
+            <table>
+                <tr>
+                    <th>Course</th>
+                    <th>Tees</th>
+                    <th>Rating</th>
+                    <th>Slope</th>
+                    <th>Delete</th>
+                </tr>
+                {tableRows}
+            </table>
+        );
+    }
+}
+
+
+class TabButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    handleOnClick(e) {
+        this.props.handleOnClick(e.target.value);
     }
 
     render() {
         return (
-            <div>
-                <SearchBar filterText={this.state.filterText} onFilterChange={this.handleOnFilterChange} inStockOnly={this.state.inStockOnly} onCheckChange={this.handleCheckChanged}/>
-                <ProductTable rawData={this.state.rawData} filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} />
-            </div>
+            <button type="button" onClick={this.handleOnClick} value={this.props.value}>
+                {this.props.value}
+            </button>
         );
     }
 }
 
-class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleFilterChange = this.handleFilterChange.bind(this);
-        this.handleCheckChange = this.handleCheckChange.bind(this);
-    }
-
-    handleFilterChange(e) {
-        this.props.onFilterChange(e.target.value);
-    }
-
-    handleCheckChange(e) {
-        this.props.onCheckChange(e.target.checked);
-    }
-
-    render(props) {
-        return (
-            <div>
-                <input
-                    type="text"
-                    value={this.props.filterText}
-                    placeholder="Search..."
-                    onChange={this.handleFilterChange}
-                />
-                <br />
-                <input type="checkbox" checked={this.props.inStockOnly} onChange={this.handleCheckChange}/> Only show products in stock
-            </div>
-        );
-    }
-}
-
-class ProductTable extends React.Component {
-    render(props) {
-        const rows = [];
-        let previousCategory;
-
-        this.props.rawData.forEach(product => {
-            if (product.name.includes(this.props.filterText) && (this.props.inStockOnly ? product.stocked : true)) {
-                if (product.category !== previousCategory) {
-                    rows.push(
-                        <ProductCategoryRow category={product.category} key={product.category} />
-                    );
-                    previousCategory = product.category;
-                }
-                rows.push(
-                    <ProductRow product={product} key={product.name} />
-                );
-            }
-        }
-        );
-
-        return (
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {rows}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
-}
-
-class ProductCategoryRow extends React.Component {
-    render(props) {
-        return (
-            <tr className="ProductCategoryRow">
-                <td colSpan="2">{this.props.category}</td>
-            </tr>
-        );
-    }
-}
-
-class ProductRow extends React.Component {
-    render(props) {
-        return (
-            <tr>
-                <td className={(this.props.product.stocked ? "" : "OutOfStock")}>{this.props.product.name}</td>
-                <td>{this.props.product.price}</td>
-            </tr>
-        );
-    }
-}
-
-const RAWDATA = [
-    { category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football" },
-    { category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball" },
-    { category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball" },
-    { category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch" },
-    { category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5" },
-    { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
-];
+const COURSES = [
+    { "course": "Gleneagles", "tee": "blue", "rating": "70.2", "slope": "123" },
+    { "course": "HG Creek/Hills", "tee": "white", "rating": "68.2", "slope": "119" },
+    { "course": "Harvest ", "tee": "blue", "rating": "70.3", "slope": "119" },
+    { "course": "Heather Glen Creek/Hills", "tee": "white", "rating": "68.2", "slope": "119" },
+    { "course": "Kamloops G&CC", "tee": "gold", "rating": "70.2", "slope": "122" },
+    { "course": "Maple Ridge", "tee": "blue", "rating": "68.8", "slope": "115" },
+    { "course": "Possum Trot", "tee": "White", "rating": "70.4", "slope": "118" },
+    { "course": "Radium Springs", "tee": "Blue", "rating": "69.7", "slope": "120" },
+    { "course": "Red Deer GCC", "tee": "white", "rating": "74.9", "slope": "135" }
+]
 
 ReactDOM.render(
-    <FilterableProductTable rawData={RAWDATA} />,
+    <HandicApp courses={COURSES} />,
     document.getElementById('root')
 );
